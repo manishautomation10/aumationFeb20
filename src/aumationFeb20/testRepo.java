193e9f783130aa0a9bc63b09c8f78bc216c9c6e0
manish-zope <zope.lucky@gmail.com>
2020-09-19 18:06:43 +0530

    changes of OM

--- src/aumationFeb20/testRepo.java
+++ src/aumationFeb20/testRepo.java
@@ -4,7 +4,7 @@ public class testRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		System.out.println("Hi");
 		System.out.println("Sir..");
 	}
 
