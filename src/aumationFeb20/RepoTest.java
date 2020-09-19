23d04d30ebeb3ca51f0796516cde8040b7edee34
manish-zope <zope.lucky@gmail.com>
2020-09-19 18:37:59 +0530

    demo branch

--- src/aumationFeb20/RepoTest.java
+++ src/aumationFeb20/RepoTest.java
@@ -4,7 +4,7 @@ public class RepoTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome balaji sir");
+		System.out.println("Welcome manish sir");
 	}
 
 }
