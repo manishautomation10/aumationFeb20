f8366fa7d9397c167d388436ae9181e436ce28d6
manish-zope <zope.lucky@gmail.com>
2020-09-19 18:22:23 +0530

    revert OM changes by yogesh

--- src/aumationFeb20/testRepo.java
+++ src/aumationFeb20/testRepo.java
@@ -4,8 +4,9 @@ public class testRepo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hi");
+		System.out.println("Hello");
 		System.out.println("Sir..");
+	
 	}
 
 }
